6716b440aa72136a40fc7bc96b47dc3e05cca5e9
Shivansh Vij <shivanshvij@outlook.com>
2022-10-17 17:55:05 -0400

    Adding fasthttp endpoint

---
+++ adapters/fasthttp/request.go
@@ -0,0 +1,38 @@
+/*
+	Copyright 2022 Loophole Labs
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package fasthttp
+
+import (
+	"github.com/loopholelabs/scale-go/runtime"
+	"github.com/loopholelabs/scale-go/runtime/generated"
+	"github.com/valyala/fasthttp"
+	"strings"
+)
+
+func FromRequestContext(ctx *runtime.Context, fastCTX *fasthttp.RequestCtx) {
+	ctx.Context.Request.Protocol = "HTTP/1.1"
+	ctx.Context.Request.Method = string(fastCTX.Request.Header.Method())
+	ctx.Context.Request.IP = fastCTX.RemoteAddr().String()
+	ctx.Context.Request.ContentLength = int64(fastCTX.Request.Header.ContentLength())
+	ctx.Context.Request.Body = fastCTX.Request.Body()
+
+	fastCTX.Request.Header.VisitAll(func(key []byte, value []byte) {
+		ctx.Context.Request.Headers[string(key)] = &generated.StringList{
+			Value: strings.Split(string(value), ","),
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -7,10 +7,14 @@ require (
 	github.com/loopholelabs/polyglot-go v0.5.0
 	github.com/stretchr/testify v1.7.5
 	github.com/tetratelabs/wazero v1.0.0-pre.2
+	github.com/valyala/fasthttp v1.40.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
