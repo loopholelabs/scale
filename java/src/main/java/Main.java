28a3f0a88fc34e926c5a6a507d964d9ce0a59cde
Jimmy Moore <jamesmoore@loopholelabs.io>
2022-11-03 17:46:15 +0000

    Updated to using wasi version, now only standard wasi stuff required in runtime

---
+++ java/src/main/java/Main.java
@@ -0,0 +1,5 @@
+// As of this writing, TeaVM-WASI requires a class with a main method, which we
+// provide here, although we do not expect it to be run.
+public class Main {
+    public static void main(String[] args) {}
+}
