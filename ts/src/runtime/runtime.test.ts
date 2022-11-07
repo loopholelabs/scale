ad7d7bb75c0ac8302e1e895c1b40597f5e31c000
Jimmy Moore <jamesmoore@loopholelabs.io>
2022-11-07 14:54:08 +0000

    Changed to support runtime([]modules).run()

--- ts/src/runtime/runtime.test.ts
+++ ts/src/runtime/runtime.test.ts
@@ -19,6 +19,7 @@ import * as fs from "fs";
 import { Module } from "./module";
 import { Context, Request, Response, StringList } from "./generated/generated";
 import { Context as OurContext } from "./context";
+import { Runtime } from "./runtime";
 
 window.TextEncoder = TextEncoder;
 window.TextDecoder = TextDecoder as typeof window["TextDecoder"];
@@ -52,7 +53,6 @@ describe("runtime", () => {
       "./example_modules/http-middleware.wasm"
     );
 
-
     //
     // TODO: Encapsulate the module chain into a runtime with single entry point.
     // Runtime r = new Runtime([]Module);
@@ -69,6 +69,66 @@ describe("runtime", () => {
     const ctx = new OurContext(context);
 
     const retContext = moduleHttpMiddleware.run(ctx);
+
+    expect(retContext).not.toBeNull();
+
+    if (retContext != null) {
+      // check the returns...
+
+      const dec = new TextDecoder();
+      const bodyText = dec.decode(retContext.context().Response.Body);
+
+      // The http-endpoint.wasm module copies the request body to the response body.
+      expect(bodyText).toBe("Hello world this is a request body");
+
+      // The http-middleware.wasm adds a header
+      const middle = retContext.context().Response.Headers.get("MIDDLEWARE");
+      expect(middle).toBeDefined();
+      const vals = middle?.Value;
+      if (vals !== undefined) {
+        expect(vals.length).toBe(1);
+        expect(vals[0]).toBe("TRUE");
+      }
+    }
+  });
+
+  it("Can run a simple e2e using runtime", () => {
+    // Create a context to send in...
+    const enc = new TextEncoder();
+    const body = enc.encode("Hello world this is a request body");
+    const headers = new Map<string, StringList>();
+    headers.set("content", new StringList(["hello"]));
+    const req1 = new Request(
+      "GET",
+      BigInt(100),
+      "https",
+      "1.2.3.4",
+      body,
+      headers
+    );
+    const respBody = enc.encode("Response body");
+    const respHeaders = new Map<string, StringList>();
+    const resp1 = new Response(200, respBody, respHeaders);
+    const context = new Context(req1, resp1);
+
+    // Now we can use context with a couple of wasm modules...
+
+    const modHttpEndpoint = fs.readFileSync(
+      "./example_modules/http-endpoint.wasm"
+    );
+    const modHttpMiddleware = fs.readFileSync(
+      "./example_modules/http-middleware.wasm"
+    );
+
+    const moduleHttpEndpoint = new Module(modHttpEndpoint, null);
+    const moduleHttpMiddleware = new Module(modHttpMiddleware, null);
+    const runtime = new Runtime([moduleHttpMiddleware, moduleHttpEndpoint]);
+
+    // Run the modules...
+
+    const ctx = new OurContext(context);
+
+    const retContext = runtime.run(ctx);
 
     expect(retContext).not.toBeNull();
 
---
+++ ts/src/runtime/runtime.ts
@@ -0,0 +1,41 @@
+/*
+	Copyright 2022 Loophole Labs
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+import { Module } from "./module";
+import { Context } from "./context";
+
+export class Runtime {
+  private _modules: Module[] = [];
+
+  constructor(mods: Module[]) {
+    let prevModule: Module | null = null;
+    mods.forEach((m: Module) => {
+      this._modules.push(m);
+      if (prevModule != null) {
+        prevModule.setNext(m);
+      }
+      prevModule = m;
+    });
+    this._modules = mods;
+  }
+
+  run(context: Context): Context | null {
+    // Run the first module only,
+    // which will be already chained to the others
+    if (this._modules.length === 0) return context;
+    return this._modules[0].run(context);
+  }
+}
