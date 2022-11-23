52ef803054a4b57012cfa4e32b65f172e698a2b5
Daniel Phillips <daniel@loopholelabs.io>
2022-11-23 15:02:22 -0600

    add basics to generated scalefile for rust

--- scalefile/scalefile.go
+++ scalefile/scalefile.go
@@ -44,6 +44,7 @@ type Language string
 const (
 	// Go is the Golang Source Language for Scale Functions
 	Go Language = "go"
+	Rust Language = "rust"
 )
 
 var (
@@ -51,7 +52,7 @@ var (
 	AcceptedVersions = []Version{V1Alpha}
 
 	// AcceptedLanguages is an array of acceptable Languages
-	AcceptedLanguages = []Language{Go}
+	AcceptedLanguages = []Language{Go, Rust}
 )
 
 // Dependency outlines the Dependency of a Scale Function
