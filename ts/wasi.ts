76e1c3a45b560663cd659326c62d211b34ca6caa
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-02-17 11:27:31 +0000

    Updated typescript guest wasi to passthrough stdout/stderr from scale function

--- ts/wasi.ts
+++ ts/wasi.ts
@@ -14,158 +14,157 @@
         limitations under the License.
 */
 
-const ErrNoInstance = Error("no webassembly instance")
-const ErrNoMemory = Error("no exported memory in webassembly instance")
+import { TextDecoder } from "util";
 
-export interface RequiredFunctions extends WebAssembly.ModuleImports {
-    fd_prestat_get(fd: number, bufPtr: number): number
-    fd_prestat_dir_name(fd: number, pathPtr: number, pathLen: number): number
-    environ_sizes_get(environCount: number, environBufSize: number): number
-    environ_get(environ: number, environBuf: number): number
-    args_sizes_get(argc: number, argvBufSize: number): number
-    args_get(argv: number, argvBuf: number): number
-    fd_filestat_get(fd: number, bufPtr: number): number
-    fd_fdstat_get(fd: number, bufPtr: number): number
-    fd_read(fd: number, iovs_ptr: number, iovs_len: number, nread_ptr: number): number
-    fd_write(fd: number, iovs: number, iovsLen: number, nwritten: number): number
-    fd_close(fd: number): number
-    fd_seek(fd: number, offset: number, whence: number, newOffsetPtr: number): number
-    poll_oneoff(sin: number, sout: number, nsubscriptions: number, nevents: number): number
-    proc_exit(rval: number): number
-    path_open(fd: number, dirflags: number, path_ptr: number, path_len: number, oflags: number, fs_rights_base: number, fs_rights_inheriting: number, fd_flags: number, opened_fd_ptr: number): number
-    clock_time_get(id: number, precision: BigInt, time: number): number
+const WASI_ESUCCESS = 0;
+const WASI_EBADF = 8;
+const WASI_EINVAL = 28;
+const WASI_ENOSYS = 52;
+
+class disabledWASI {
+  private instance: WebAssembly.Instance | undefined;
+
+  public setInstance(i: WebAssembly.Instance) {
+    this.instance = i;
+  }
+
+  fd_write(fd: number, iovs: number, iovsLen: number, nwritten: number) {
+    if (this.instance == undefined) {
+      return WASI_EBADF;
+    }
+    // Look at the memory, and update the number written...
+    const mem = this.instance.exports.memory as WebAssembly.Memory;
+    const memData = new Uint8Array(mem.buffer);
+
+    // Get the io vectors
+    let bytesWritten = 0;
+    let iovs_ptr = iovs;
+
+    for (let vec = 0;vec < iovsLen; vec ++) {
+      const v = memData.slice(iovs_ptr, iovs_ptr + 8);
+      const dv = new DataView(v.buffer);
+      const ptr = dv.getUint32(0, true);
+      const len = dv.getUint32(4, true);
+      iovs_ptr += 8;
+      bytesWritten += len;
+
+      const b = memData.slice(ptr, ptr + len);
+      const dec = new TextDecoder();
+      //const hex = [...b].map(x => x.toString(16).padStart(2, '0')).join('');
+
+      // NOTE: The console.log/error functions add a newline at the end.
+      if (fd == 1) {
+        console.log(dec.decode(b));
+      } else if (fd == 2) {
+        console.error(dec.decode(b));
+      }
+    }
+
+    new DataView(mem.buffer).setUint32(nwritten, bytesWritten, true);
+    return WASI_ESUCCESS;
+  }
+
+  fd_prestat_get(fd: number, bufPtr: number) {
+    return WASI_EBADF;
+  }
+
+  fd_prestat_dir_name(fd: number, pathPtr: number, pathLen: number) {
+    return WASI_EINVAL;
+  }
+
+  environ_sizes_get(environCount: number, environBufSize: number) {
+    return WASI_ESUCCESS;
+  }
+
+  environ_get(environ: number, environBuf: number) {
+    return WASI_ESUCCESS;
+  }
+
+  args_sizes_get(argc: number, argvBufSize: number) {
+    return WASI_ESUCCESS;
+  }
+
+  args_get(argv: number, argvBuf: number) {
+    return WASI_ESUCCESS;
+  }
+
+  fd_filestat_get() {
+    return WASI_ENOSYS;
+  }
+
+  fd_fdstat_get(fd: number, bufPtr: number) {
+    return WASI_ESUCCESS;
+  }
+
+  fd_read() {
+    return WASI_ESUCCESS;
+  }
+
+  poll_oneoff(sin: number, sout: number, nsubscriptions: number, nevents: number) {
+    return WASI_ENOSYS;
+  }
+
+  proc_exit(rval: number) {
+    return WASI_ENOSYS;
+  }
+
+  fd_close(fd: number) {
+    return WASI_ENOSYS;
+  }
+
+  fd_seek(fd: number, offset: number, whence: number, newOffsetPtr: number) {
+  }
+
+  path_open() {
+    return WASI_ESUCCESS;
+  }
+
+  clock_time_get() {
+    return WASI_ESUCCESS;
+  }
+
+  public imports(): any {
+    return {
+        environ_sizes_get: this.environ_sizes_get.bind(this),
+        args_sizes_get: this.args_sizes_get.bind(this),
+        fd_prestat_get: this.fd_prestat_get.bind(this),
+        fd_fdstat_get: this.fd_fdstat_get.bind(this),
+        fd_filestat_get: this.fd_filestat_get.bind(this),
+        fd_read: this.fd_read.bind(this),
+        fd_write: this.fd_write.bind(this),
+        fd_prestat_dir_name: this.fd_prestat_dir_name.bind(this),
+        path_open: this.path_open.bind(this),
+        environ_get: this.environ_get.bind(this),
+        args_get: this.args_get.bind(this),
+        poll_oneoff: this.poll_oneoff.bind(this),
+        proc_exit: this.proc_exit.bind(this),
+        fd_close: this.fd_close.bind(this),
+        fd_seek: this.fd_seek.bind(this),
+        clock_time_get: this.clock_time_get.bind(this),
+    }
+  }
 }
 
-export class DisabledWASI {
-    public static ESUCCESS = 0;
-    public static EBADF = 8;
-    public static EINVAL = 28;
-    public static ENOSYS = 52;
 
-    public static CLOCKID_REALTIME = 0;
+interface WASIContext {
+    start(instance: WebAssembly.Instance): void;
+    getImportObject(): any;
+}
 
-    private exports: WebAssembly.Exports | undefined;
+export function NewWASI(): WASIContext {
+    let w = new disabledWASI();
 
-    private setBigUint64(view: DataView, byteOffset: number, value: number, littleEndian: boolean) {
-        const lowWord = value;
-        const highWord = 0;
-        view.setUint32(littleEndian ? 0 : 4, lowWord, littleEndian);
-        view.setUint32(littleEndian ? 4 : 0, highWord, littleEndian);
-    }
+    return {
+      getImportObject: () => {
+          return w.imports();
+      },
+      start: (instance: WebAssembly.Instance) => {
+          w.setInstance(instance);
 
-    private getDataView(): DataView {
-        if (!this.exports) {
-            throw ErrNoInstance;
-        }
-        if (!this.exports.memory) {
-            throw ErrNoMemory;
-        }
-        // @ts-ignore
-        return new DataView(this.exports.memory.buffer);
-    }
-
-    public SetInstance(instance: WebAssembly.Instance) {
-        this.exports = instance.exports;
-        const start = (this.exports._start as Function | undefined);
-        if (start) {
-            start();
-        }
-    }
-
-    public environ_sizes_get(environCount: number, environBufSize: number): number {
-        let view = this.getDataView();
-        view.setUint32(environCount, 0, true);
-        view.setUint32(environBufSize, 0, true);
-        return DisabledWASI.ESUCCESS;
-    }
-
-    public environ_get(environ: number, environBuf: number): number {
-        return DisabledWASI.ESUCCESS;
-    }
-
-    public args_sizes_get(argc: number, argvBufSize: number): number {
-        let view = this.getDataView();
-        view.setUint32(argc, 0, true);
-        view.setUint32(argvBufSize, 0, true);
-        return DisabledWASI.ESUCCESS;
-    }
-
-    public args_get(argv: number, argvBuf: number): number {
-        return DisabledWASI.ESUCCESS;
-    }
-
-    public fd_prestat_get(fd: number, bufPtr: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public fd_prestat_dir_name(fd: number, pathPtr: number, pathLen: number): number {
-        return DisabledWASI.EINVAL;
-    }
-
-    public fd_filestat_get(fd: number, bufPtr: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public fd_fdstat_get(fd: number, bufPtr: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public fd_read(fd: number, iovs_ptr: number, iovs_len: number, nread_ptr: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public fd_write(fd: number, iovs: number, iovsLen: number, nwritten: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public fd_close(fd: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public fd_seek(fd: number, offset: number, whence: number, newOffsetPtr: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public poll_oneoff(sin: number, sout: number, nsubscriptions: number, nevents: number): number {
-        return DisabledWASI.ENOSYS;
-    }
-
-    public proc_exit(rval: number): number {
-        return DisabledWASI.ENOSYS;
-    }
-
-    public path_open(fd: number, dirflags: number, path_ptr: number, path_len: number, oflags: number, fs_rights_base: number, fs_rights_inheriting: number, fd_flags: number, opened_fd_ptr: number): number {
-        return DisabledWASI.EBADF;
-    }
-
-    public clock_time_get(id: number, precision: BigInt, time: number) {
-        let buffer = this.getDataView()
-        if (id === DisabledWASI.CLOCKID_REALTIME) {
-            buffer.setBigUint64(time, BigInt(new Date().getTime()) * 1000000n, true);
-        } else {
-            buffer.setBigUint64(time, 0n, true);
-        }
-        return DisabledWASI.ESUCCESS;
-    }
-
-    public GetImports(): RequiredFunctions {
-        return {
-            environ_sizes_get: this.environ_sizes_get.bind(this),
-            environ_get: this.environ_get.bind(this),
-            args_sizes_get: this.args_sizes_get.bind(this),
-            args_get: this.args_get.bind(this),
-            fd_prestat_get: this.fd_prestat_get.bind(this),
-            fd_prestat_dir_name: this.fd_prestat_dir_name.bind(this),
-            fd_filestat_get: this.fd_filestat_get.bind(this),
-            fd_fdstat_get: this.fd_fdstat_get.bind(this),
-            fd_read: this.fd_read.bind(this),
-            fd_write: this.fd_write.bind(this),
-            fd_close: this.fd_close.bind(this),
-            fd_seek: this.fd_seek.bind(this),
-            poll_oneoff: this.poll_oneoff.bind(this),
-            proc_exit: this.proc_exit.bind(this),
-            path_open: this.path_open.bind(this),
-            clock_time_get: this.clock_time_get.bind(this),
-        }
-    }
+          const startFn = (instance.exports._start as Function | undefined);
+          if (startFn) {
+              startFn();
+          }
+      }
+  };
 }
