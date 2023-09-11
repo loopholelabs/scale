219edf9cdd4acac44d48c1fdf4ae4146d69e47e8
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-09-11 10:27:08 +0100

    Initial extension generator code
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

---
+++ extension/schema_test.go
@@ -0,0 +1,48 @@
+//go:build !integration
+
+/*
+	Copyright 2023 Loophole Labs
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package extension
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSchema(t *testing.T) {
+	s := new(Schema)
+	err := s.Decode([]byte(MasterTestingSchema))
+	require.NoError(t, err)
+
+	require.NoError(t, s.Validate())
+
+	assert.Equal(t, V1AlphaVersion, s.Version)
+	assert.Equal(t, "HttpFetch", s.Name)
+	assert.Equal(t, "alpha", s.Tag)
+
+	// Make sure there's a global function defined...
+	assert.Equal(t, 1, len(s.Functions))
+
+	assert.Equal(t, "New", s.Functions[0].Name)
+	assert.Equal(t, "HttpConfig", s.Functions[0].Params)
+	assert.Equal(t, "HttpConnector", s.Functions[0].Return)
+
+	// NB You could test the models, but that should be done in signature already.
+
+}
