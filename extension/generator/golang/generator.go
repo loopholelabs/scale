254c8aad6a0d2accf2f45aad33e080971739ca0f
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-09-11 10:27:08 +0100

    Initial extension generator code
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

---
+++ extension/generator/golang/generator.go
@@ -0,0 +1,274 @@
+/*
+	Copyright 2023 Loophole Labs
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+		   http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package golang
+
+import (
+	"bytes"
+	"go/format"
+	"text/template"
+
+	"github.com/loopholelabs/scale/extension"
+
+	"github.com/loopholelabs/scale/extension/generator/templates"
+	"github.com/loopholelabs/scale/signature/generator/utils"
+)
+
+const (
+	defaultPackageName = "types"
+)
+
+var generator *Generator
+
+func Generate(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	return generator.Generate(schema, packageName, version)
+}
+
+func GenerateInterfaces(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	return generator.GenerateInterfaces(schema, packageName, version)
+}
+
+func GenerateGuest(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	return generator.GenerateGuest(schema, packageName, version)
+}
+
+func GenerateModfile(packageName string, polyglotVersion string) ([]byte, error) {
+	return generator.GenerateModfile(packageName, polyglotVersion)
+}
+
+func GenerateHost(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	return generator.GenerateHost(schema, packageName, version)
+}
+
+func init() {
+	var err error
+	generator, err = New()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Generator is the go generator
+type Generator struct {
+	templ *template.Template
+}
+
+// New creates a new go generator
+func New() (*Generator, error) {
+	templ, err := template.New("").Funcs(templateFunctions()).ParseFS(templates.FS, "*.go.templ")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Generator{
+		templ: templ,
+	}, nil
+}
+
+// Generate generates the go code
+func (g *Generator) Generate(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	if packageName == "" {
+		packageName = defaultPackageName
+	}
+
+	buf := new(bytes.Buffer)
+	err := g.templ.ExecuteTemplate(buf, "types.go.templ", map[string]any{
+		"schema":  schema,
+		"version": version,
+		"package": packageName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return format.Source(buf.Bytes())
+}
+
+func (g *Generator) GenerateInterfaces(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	if packageName == "" {
+		packageName = defaultPackageName
+	}
+
+	buf := new(bytes.Buffer)
+	err := g.templ.ExecuteTemplate(buf, "interfaces.go.templ", map[string]any{
+		"schema":  schema,
+		"version": version,
+		"package": packageName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return format.Source(buf.Bytes())
+}
+
+// GenerateGuest generates the guest bindings
+func (g *Generator) GenerateGuest(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	if packageName == "" {
+		packageName = defaultPackageName
+	}
+
+	buf := new(bytes.Buffer)
+	err := g.templ.ExecuteTemplate(buf, "guest.go.templ", map[string]any{
+		"schema":  schema,
+		"version": version,
+		"package": packageName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return format.Source(buf.Bytes())
+}
+
+// GenerateModfile generates the modfile for the signature
+func (g *Generator) GenerateModfile(packageName string, polyglotVersion string) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	err := g.templ.ExecuteTemplate(buf, "mod.go.templ", map[string]any{
+		"polyglot_version": polyglotVersion,
+		"package":          packageName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// GenerateHost generates the host bindings
+func (g *Generator) GenerateHost(schema *extension.Schema, packageName string, version string) ([]byte, error) {
+	if packageName == "" {
+		packageName = defaultPackageName
+	}
+
+	buf := new(bytes.Buffer)
+	err := g.templ.ExecuteTemplate(buf, "host.go.templ", map[string]any{
+		"schema":  schema,
+		"version": version,
+		"package": packageName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return format.Source(buf.Bytes())
+}
+
+func templateFunctions() template.FuncMap {
+	return template.FuncMap{
+		"IsInterface":             isInterface,
+		"Primitive":               primitive,
+		"IsPrimitive":             extension.ValidPrimitiveType,
+		"PolyglotPrimitive":       polyglotPrimitive,
+		"PolyglotPrimitiveEncode": polyglotPrimitiveEncode,
+		"PolyglotPrimitiveDecode": polyglotPrimitiveDecode,
+		"Deref":                   func(i *bool) bool { return *i },
+		"LowerFirst":              func(s string) string { return string(s[0]+32) + s[1:] },
+		"Params":                  utils.Params,
+	}
+}
+
+func isInterface(schema *extension.Schema, s string) bool {
+	for _, i := range schema.Interfaces {
+		if i.Name == s {
+			return true
+		}
+	}
+	return false
+}
+
+func primitive(t string) string {
+	switch t {
+	case "string", "int32", "int64", "uint32", "uint64", "float32", "float64", "bool":
+		return t
+	case "bytes":
+		return "[]byte"
+	default:
+		return ""
+	}
+}
+
+func polyglotPrimitive(t string) string {
+	switch t {
+	case "string":
+		return "polyglot.StringKind"
+	case "int32":
+		return "polyglot.Int32Kind"
+	case "int64":
+		return "polyglot.Int64Kind"
+	case "uint32":
+		return "polyglot.Uint32Kind"
+	case "uint64":
+		return "polyglot.Uint64Kind"
+	case "float32":
+		return "polyglot.Float32Kind"
+	case "float64":
+		return "polyglot.Float64Kind"
+	case "bool":
+		return "polyglot.BoolKind"
+	case "bytes":
+		return "polyglot.BytesKind"
+	default:
+		return "polyglot.AnyKind"
+	}
+}
+
+func polyglotPrimitiveEncode(t string) string {
+	switch t {
+	case "string":
+		return "String"
+	case "int32":
+		return "Int32"
+	case "int64":
+		return "Int64"
+	case "uint32":
+		return "Uint32"
+	case "uint64":
+		return "Uint64"
+	case "float32":
+		return "Float32"
+	case "float64":
+		return "Float64"
+	case "bool":
+		return "Bool"
+	case "bytes":
+		return "Bytes"
+	default:
+		return ""
+	}
+}
+
+func polyglotPrimitiveDecode(t string) string {
+	switch t {
+	case "string":
+		return "String"
+	case "int32":
+		return "Int32"
+	case "int64":
+		return "Int64"
+	case "uint32":
+		return "Uint32"
+	case "uint64":
+		return "Uint64"
+	case "float32":
+		return "Float32"
+	case "float64":
+		return "Float64"
+	case "bool":
+		return "Bool"
+	case "bytes":
+		return "Bytes"
+	default:
+		return ""
+	}
+}
---
+++ extension/generator/golang/generator_test.go
@@ -0,0 +1,65 @@
+//go:build !integration
+
+/*
+	Copyright 2023 Loophole Labs
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+		   http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package golang
+
+import (
+	"os"
+	"testing"
+
+	"github.com/loopholelabs/scale/extension"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerator(t *testing.T) {
+	s := new(extension.Schema)
+	err := s.Decode([]byte(extension.MasterTestingSchema))
+	require.NoError(t, err)
+
+	require.NoError(t, s.Validate())
+
+	packageName := "extfetch"
+
+	f_interfaces, err := GenerateInterfaces(s, packageName, "v0.1.0")
+	require.NoError(t, err)
+	os.WriteFile("./interfaces.txt", f_interfaces, 0644)
+
+	formatted, err := Generate(s, "types", "v0.1.0")
+	require.NoError(t, err)
+
+	// Check things...
+
+	os.WriteFile("./generated.txt", formatted, 0644)
+
+	master, err := os.ReadFile("./generated.txt")
+	require.NoError(t, err)
+	require.Equal(t, string(master), string(formatted))
+
+	f_host, err := GenerateHost(s, packageName, "v0.1.0")
+	require.NoError(t, err)
+
+	os.WriteFile("./host.txt", f_host, 0644)
+
+	f_guest, err := GenerateGuest(s, packageName, "v0.1.0")
+	require.NoError(t, err)
+
+	os.WriteFile("./guest.txt", f_guest, 0644)
+
+	modf, err := GenerateModfile(packageName, "v0.1.0")
+	require.NoError(t, err)
+
+	os.WriteFile("./modfile.txt", modf, 0644)
+
+}
