1d7da613d81264b6c6799281f1e05fbad3bcf874
Daniel Phillips <daniel@loopholelabs.io>
2024-07-24 17:57:13 -0500

    Add net/http adapter ex

--- examples/advanced/serverless_framework/scale-server/go.mod
+++ examples/advanced/serverless_framework/scale-server/go.mod
@@ -1,3 +1,21 @@
 module my-go-server
 
 go 1.22.2
+
+require (
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/hashicorp/hcl/v2 v2.18.1 // indirect
+	github.com/loopholelabs/polyglot v1.1.3 // indirect
+	github.com/loopholelabs/polyglot-go v0.5.1 // indirect
+	github.com/loopholelabs/scale v0.4.8 // indirect
+	github.com/loopholelabs/scale-extension-interfaces v0.0.0-20230920094333-3a483b301bf4 // indirect
+	github.com/loopholelabs/scale-signature-interfaces v0.1.7 // indirect
+	github.com/loopholelabs/scalefile v0.1.9 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/tetratelabs/wazero v1.5.0 // indirect
+	github.com/zclconf/go-cty v1.14.1 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
--- examples/advanced/serverless_framework/scale-server/main.go
+++ examples/advanced/serverless_framework/scale-server/main.go
@@ -1,43 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
+  "fmt"
+  "log"
+	"embed"
+	"io"
+  "net/http"
+	scale "github.com/loopholelabs/scale/go"
+	adapter "github.com/loopholelabs/scale-http-adapters/http"
+  "github.com/loopholelabs/scalefile/scalefunc"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodOptions {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-        w.WriteHeader(http.StatusOK)
-        return
-    }
-
-    if r.Method == http.MethodPost {
-        body, err := ioutil.ReadAll(r.Body)
-        if err != nil {
-            http.Error(w, "Unable to read request body", http.StatusBadRequest)
-            return
-        }
-        defer r.Body.Close()
-
-        // Log the received code
-        fmt.Printf("Received code: %s\n", string(body))
-
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte("Code received successfully"))
-    } else {
-        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-    }
-}
+//go:embed local-serverless-latest.scale
+var embeddedFunction []byte
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Server is listening on port 3000...")
+	sf := new(ScaleFunc)
+	_ = sf.Decode(embeddedFunction)
+
+	r, _ := scale.New(context.Background(), []*scalefunc.ScaleFunc{sf})
+    handler := adapter.New(nil, r)
+
+    http.Handle("/", handler)
     log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
